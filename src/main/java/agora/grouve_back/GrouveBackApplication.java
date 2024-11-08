bb37785738e5508b19ac1790703212154be3868c
HanSuBin <han04245@naver.com>
2024-11-09 00:18:44 +0900

    프로젝트 생성했,mysql 연결해야함

---
+++ src/main/java/agora/grouve_back/GrouveBackApplication.java
@@ -0,0 +1,13 @@
+package agora.grouve_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrouveBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrouveBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/agora/grouve_back/GrouveBackApplicationTests.java
@@ -0,0 +1,13 @@
+package agora.grouve_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrouveBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
